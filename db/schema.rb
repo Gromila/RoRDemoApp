6625631b594e52602ac609483ab1823f7b754238
Gromila <gromilich@gmail.com>
2014-01-04 18:42:56 +0300

    Finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140104151030) do
+ActiveRecord::Schema.define(version: 20140104152731) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
